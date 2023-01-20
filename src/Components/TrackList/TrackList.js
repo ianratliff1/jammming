fb4c50519f3731a31d723ac18c21f863a71bcc1e
Ian Ratliff <112771370+ianratliff1@users.noreply.github.com>
2023-01-20 11:40:58 +0100

    Initial commit

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './TrackList.css';
+
+class TrackList extends React.Component {
+    render() {
+        return (
+            <div className="TrackList">
+                Creed - Take me Higher <br />
+                Nickelback - Animals <br />
+                Kanye West - Stupido <br />
+            </div>
+        )
+    }
+}
+
+export default TrackList;
