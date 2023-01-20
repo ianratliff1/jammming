c4e91667f56f936cf7ca62ae277bfc5ed76603b1
Ian Ratliff <112771370+ianratliff1@users.noreply.github.com>
2023-01-20 21:54:23 +0100

    Through step 80

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,24 +6,52 @@ import Playlist from '../Playlist/Playlist';
 import SearchResults from '../SearchResults/SearchResults';
 // import TrackList from '../TrackList/TrackList';
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchResults: [{ name: 'name1', artist: 'artist1', album: 'album1', id: 1 },{ name: 'name2', artist: 'artist2', album: 'album2', id: 2 },{ name: 'name3',artist: 'artist3', album: 'album3', id: 3 }],
+      searchResults: [{ name: 'name1', artist: 'artist1', album: 'album1', id: 1 }, { name: 'name2', artist: 'artist2', album: 'album2', id: 2 }, { name: 'name3', artist: 'artist3', album: 'album3', id: 3 }],
       playlistName: 'My Playlist',
-      playlistTracks: [{ name: 'PLname1', artist: 'PLartist1', album: 'PLalbum1', id: 4 },{ name: 'PLname2', artist: 'PLartist2', album: 'PLalbum2', id: 5 },{ name: 'PLname3',artist: 'PLartist3', album: 'PLalbum3', id: 6 }]
+      playlistTracks: [{ name: 'Diced Pineapples', artist: 'Rick Ross, Wale, Drake', album: 'God Forgives, I Don\'t (Deluxe Edition)', id: 4, uri: '27mF2eUbhExYWiOT0y9cuc' }, { name: 'Keep On', artist: 'Alfa Mist', album: 'Antiphon', id: 5, uri: '1GzBTeJLSh6frMPWxvNygc' }, { name: 'Suede', artist: 'NxWorries, Anderson .Paak, Knxwledge', album: 'Yes Lawd!', id: 6, uri: '0iizrCBnUUJuAwCPJaWWY0' }]
     }
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
     let tracks = this.state.playlistTracks;
-    if(tracks.find(savedTrack => savedTrack.id == track.id)) {
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
       return;
     }
-
     tracks.push(track);
-    this.setState({playlistTracks: tracks})
+    this.setState({ playlistTracks: tracks })
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(thisTrack => thisTrack.id !== track.id);
+    this.setState({ playlistTracks: tracks });
+    console.log(tracks);
+
+  }
+
+  updatePlaylistName(name) {
+    this.setState( { playlistName: name} )
+  }
+
+  savePlaylist() {
+    let trackURIs = [];
+    let tracks = this.state.playlistTracks; 
+    tracks.forEach(track => trackURIs.push(track.uri))
+    // console.log(trackURIs)
+  }
+
+  search(searchTerm) {
+    console.log(searchTerm);
   }
 
   render() {
@@ -31,10 +59,15 @@ class App extends React.Component {
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <Playlist playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks} 
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
+            />
           </div>
         </div>
       </div>
@@ -45,25 +78,3 @@ class App extends React.Component {
 
 
 export default App;
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
