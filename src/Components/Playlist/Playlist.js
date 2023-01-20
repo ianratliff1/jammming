c2dd491a4ce4d1d2c008fa8fa3d4a0a63f5394c9
Ian Ratliff <112771370+ianratliff1@users.noreply.github.com>
2023-01-20 14:50:21 +0100

    Step 38-39 complete.
    It's been broken since step 34 (tracklist component not getting props)

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -7,7 +7,7 @@ class Playlist extends React.Component {
         return (
             <div className="Playlist">
                 <input defaultValue={'New Playlist'} />
-                <TrackList />
+                <TrackList tracks={this.props.playlistTracks} />
                 <button class="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,13 +1,16 @@
 import React from "react";
 import './TrackList.css';
+import Track from "../Track/Track";
 
 class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
-                Creed - Take me Higher <br />
-                Nickelback - Animals <br />
-                Kanye West - Stupido <br />
+                {
+                    this.props.tracks.map(track => {
+                        return <Track track={track} key={track.id} />;
+                    })
+                }
             </div>
         )
     }
